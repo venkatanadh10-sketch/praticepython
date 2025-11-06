650b494ac862b1b98011b255d416fcbaccd06129
surendra kolipaka <surendrakolipaka@surendras-MacBook-Pro.local>
2025-11-06 09:58:49 +0800

    praticcode

--- pythondev.py
+++ pythondev.py
@@ -1,2 +1,18 @@
-this devsecops session
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    name = None
+    if request.method == "POST":
+        name = request.form.get("name")
+    return render_template("index.html", name=name)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+git 
